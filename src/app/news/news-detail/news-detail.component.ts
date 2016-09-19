6aa55c95b060de68c112d46ab7b11f2b622c8b41
Nicolas Kovalsky <kovalsky.nicolas@gmail.com>
2016-09-19 12:29:17 -0300

    Agrego disqus y sharethis, y link a las imagenes

--- src/app/news/news-detail/news-detail.component.ts
+++ src/app/news/news-detail/news-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { News } from '../../shared/news.class';
 import { Subscription } from 'rxjs/Rx';
@@ -9,7 +9,7 @@ import { NewsService } from '../../shared/news.service';
   templateUrl: './news-detail.component.html',
   styles: []
 })
-export class NewsDetailComponent implements OnInit {
+export class NewsDetailComponent implements OnInit, AfterViewInit{
 
   private suscription: Subscription;
   newsItem: News;
@@ -24,7 +24,19 @@ export class NewsDetailComponent implements OnInit {
     );
   }
 
+
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    var d = document, s = d.createElement('script');
+    s.src = '//newspark-com-ar.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', '' + new Date());
+    (d.head || d.body).appendChild(s);
+
+    var os = d.createElement('script');
+    os.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57dffeff9b581af2';
+    (d.head || d.body).appendChild(os);
   }
 
 }
