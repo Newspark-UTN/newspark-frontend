5c72d0ba08980da70c6ce112d8a3ce03958eb778
Nicolas Kovalsky <kovalsky.nicolas@gmail.com>
2016-10-21 01:47:34 -0300

    Fix contact form

--- src/app/news/contact/contact.component.ts
+++ src/app/news/contact/contact.component.ts
@@ -7,12 +7,19 @@ import { NewsService } from '../../shared/news.service.ts';
   styles: []
 })
 export class ContactComponent {
-
+  mensajeEnviado: boolean = false;
+  enviando: boolean = false;
   constructor(private newsService: NewsService) {
   }
-  
+
   onSubmit(form: any): void {
     console.log('you submitted value:', form)
-    this.newsService.sendContact(form)
+    this.enviando = true;
+    this.newsService.sendContact(form).subscribe(
+      (result) => {
+        console.log(result)
+        this.mensajeEnviado = true;
+      }
+    )
   }
 }
--- src/app/shared/news.service.ts
+++ src/app/shared/news.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { News } from './news.class';
-import { Http, URLSearchParams, Jsonp } from '@angular/http';
+import { Http, URLSearchParams, Jsonp, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/Rx';
@@ -9,31 +9,34 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class NewsService {
+  private headers = new Headers({ 'Content-Type': 'application/json' });
 
   fetchedNews: News[];
   baseUrl: string = "http://api.newspark.com.ar:8080";
 
-  constructor(private http: Http, private jsonp : Jsonp) { }
+  constructor(private http: Http, private jsonp: Jsonp) { }
 
   getNews(): Observable<News[]> {
-    return this.http.get( this.baseUrl + '/news')
+    return this.http.get(this.baseUrl + '/news')
       .map((data) => data.json());
   }
 
   getNewsById(_id: String): Observable<News> {
-    return this.http.get( this.baseUrl + '/news/' + _id)
+    return this.http.get(this.baseUrl + '/news/' + _id)
       .map((data) => data.json());
   }
 
   getNewsByCategory(category: string): Observable<News[]> {
     let params = new URLSearchParams();
     params.set('category', category);
-    return this.http.get( this.baseUrl + '/news' + '?category=' + category)
+    return this.http.get(this.baseUrl + '/news' + '?category=' + category)
       .map((data) => data.json());
   }
 
-  sendContact(info : any) {
-    return this.http.post(this.baseUrl + '/contact', info)
+  sendContact(info: any) {
+    console.log(info);
+
+    return this.http.post(this.baseUrl + '/contact', JSON.stringify(info), { headers: this.headers });
   }
 
   /* save: function(todo) {
