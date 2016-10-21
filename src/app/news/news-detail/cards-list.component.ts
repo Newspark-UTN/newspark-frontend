4891ec3541f4d07cd2fe2531aabfb9263f16ac99
Alan Szpigiel <alanszp@gmail.com>
2016-10-20 23:35:56 -0300

    order articles by date & add date to each card

--- src/app/news/news-detail/cards-list.component.ts
+++ src/app/news/news-detail/cards-list.component.ts
@@ -12,4 +12,26 @@ export class CardsListComponent implements OnInit {
   ngOnInit() {
   }
 
+  sortedArticles(){
+    if (!this.newsItem) {
+        return []
+    }
+
+    return this.newsItem.articles.sort(function(a1, a2) {
+        var d1 : string = a1.articleDate
+        var d2 : string = a2.articleDate
+
+        if (d1 == d2) {
+            return 0
+        }
+        else if (d1 > d2) {
+            return -1
+        }
+        else {
+            return 1;
+        }
+
+    });
+  }
+
 }
